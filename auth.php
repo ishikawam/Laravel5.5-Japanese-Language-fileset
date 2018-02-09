4cf624a3bed0f3e232269545fdb8929ff68b4c45
M_Ishikawa <ishikawam@nifty.com>
2018-02-09 10:59:31 +0900

    README.md, typo and indent fix

--- auth.php
+++ auth.php
@@ -12,7 +12,7 @@ return [
     | messages that we need to display to the user. You are free to modify
     | these language lines according to your application's requirements.
     |
-    { 次の言語行は、ユーザーに表示する必要のあるさまざまなメッセージの認証時に使用されます。
+    | 次の言語行は、ユーザーに表示する必要のあるさまざまなメッセージの認証時に使用されます。
     | これらの言語の行は、アプリケーションの要件に応じて自由に変更できます。
     */
 
